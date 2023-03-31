31f46c021626ff77252e370df4ebf394c4deea3e
matosky <cyphercode59@gmail.com>
2023-03-31 02:26:50 +0100

    new dashboard changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
-import Dashboard from './components/dashboard/main'
 
 function App() {
   return (
-        <div className="App">
-          <Dashboard />
+      <div className="App">
+      
         </div>
   )
 }
